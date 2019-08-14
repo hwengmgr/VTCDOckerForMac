41cb741aec941c726848c3d53f929bac31aaeb4c
Larry Klein <kleinl@yahoo.com>
2019-08-14 06:12:03 -0400

    don

--- build-MacOSX-only.sh
+++ build-MacOSX-only.sh
@@ -13,10 +13,10 @@ tar xzf MacOSX10.11.sdk.tar.gz -C depends/SDKs/ --overwrite
 make -j8 -C depends HOST=$HOST
 mkdir -p $WORKSPACE/out/$HOST
 ./autogen.sh
-mkdir build && cd build
-../configure --disable-tests  --with-libs-no --prefix=$WORKSPACE/depends/$HOST --bindir=$WORKSPACE/out/$HOST/bin --libdir=$WORKSPACE/out/$HOST/lib 
+#mkdir build && cd build
+./configure --disable-tests  --with-libs-no --prefix=$WORKSPACE/depends/$HOST --bindir=$WORKSPACE/out/$HOST/bin --libdir=$WORKSPACE/out/$HOST/lib 
 make -j8 install
-cd ../
+#cd ../
 #rm -rf build
 #rm -rf /root/output/$HOST
 #mv $WORKSPACE/out/$HOST /root/output/$HOST
@@ -24,4 +24,6 @@ cd ../
 #zip /root/output/bitcoind-windows-x64.zip /root/output/x86_64-w64-mingw32/bin/bitcoind.exe /root/output/x86_64-w64-#mingw32/bin/bitcoin-tx.exe /root/output/x86_64-w64-mingw32/bin/bitcoin-cli.exe
 #zip /root/output/bitcoin-qt-windows-x64.zip /root/output/x86_64-w64-mingw32/bin/bitcoin-qt.exe
 
+echo Done at
+date
 
